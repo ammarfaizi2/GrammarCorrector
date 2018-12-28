f474922f6130c4c9a9f06d613be843140afd062c
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-28 21:34:24 +0700

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,14 @@
 <?php
 
-/**
- * @param string $str
- * @return string
- */
-function gmc_corrector(string $str): string
-{
-	return GMC_CORRECTION_FLAG.$str.GMC_CORRECTION_FLAG;
+
+
+if (!function_exists("gmc_corrector")) {
+	/**
+	 * @param string $str
+	 * @return string
+	 */
+	function gmc_corrector(string $str): string
+	{
+		return GMC_CORRECTION_FLAG.$str.GMC_CORRECTION_FLAG;
+	}
 }
